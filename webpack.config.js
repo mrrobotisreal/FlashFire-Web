5038cb1e75329096cb3e68aac24653bf24dc860e
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-05-25 12:02:54 -0600

    Edited .gitignore and webpack.config.js
    Added *.pem to .gitignore, added css-loader and style-loader to
    webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -13,11 +13,15 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx|css)$/,
+        test: /\.(js|jsx)$/,
         exclude: /nodeModules/,
         use: {
           loader: "babel-loader",
         },
+      },
+      {
+        test: /\.(css)$/i,
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
