e5f86f355206f61f00cb4b325b8d604b5c8d0a98
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-05-31 14:33:39 -0600

    Added EditMode component and gave it boilerplate code.

---
+++ client/src/components/EditMode.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class EditMode extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default EditMode;
